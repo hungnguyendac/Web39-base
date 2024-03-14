40fb5fb74d33a0b5e3665879340e5f057fe3e607
NDHung <hungnguyendac6@gmail.com>
2024-03-14 20:53:34 +0700

    Update buổi 8

--- Project_Web-tinh/assets/js/custom.js
+++ Project_Web-tinh/assets/js/custom.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+    // $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+        loop: true, // Vòng lặp
+        margin: 30, // Khoảng cách giữa các item
+        autoplay: true, // Tự động chuyển slide
+        autoplayTimeout: 6000,
+        nav: true, // Thanh điều hướng (mũi tên)
+        responsive: {
+            0: {
+                // -> 599px
+                items: 1,
+            },
+            600: {
+                // 600px -> 999px
+                items: 1,
+            },
+            1000: {
+                // 1000px trở lên
+                items: 1,
+            },
+        },
+    });
 });
